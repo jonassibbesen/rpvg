27c3b39a20f1db5077bd7019e658835a94129173
jonassibbesen <j.a.sibbesen@gmail.com>
2019-08-28 17:40:08 -0700

    small fix

--- src/alignment_path_finder.cpp
+++ src/alignment_path_finder.cpp
@@ -275,7 +275,6 @@ void AlignmentPathFinder<AlignmentType>::pairAlignmentPaths(vector<AlignmentPath
                 }
 
                 found_overlap = true;
-                break;
             }
         }
 
