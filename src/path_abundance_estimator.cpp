b0bc26d9ee82cb267a4df1ceae79cd69b7a06d76
jonassibbesen <j.a.sibbesen@gmail.com>
2020-07-23 10:21:13 -0700

    use non-normalized probs in gibbs

--- src/path_abundance_estimator.cpp
+++ src/path_abundance_estimator.cpp
@@ -281,14 +281,15 @@ void NestedPathAbundanceEstimator::estimate(PathClusterEstimates * path_cluster_
 
         for (auto & group: path_groups) {
 
-            Eigen::ColMatrixXd group_read_path_probs = Eigen::ColMatrixXd(read_path_probs.rows(), group.size());
+            Eigen::ColMatrixXd group_read_path_probs = Eigen::ColMatrixXd(read_path_probs.rows(), group.size() + 1);
 
             for (size_t i = 0; i < group.size(); ++i) {
 
                 group_read_path_probs.col(i) = read_path_probs.col(group.at(i));
             }
 
-            addNoiseAndNormalizeProbabilityMatrix(&group_read_path_probs, noise_probs);
+            // addNoiseAndNormalizeProbabilityMatrix(&group_read_path_probs, noise_probs);
+            group_read_path_probs.col(group_read_path_probs.cols() - 1) = noise_probs;
 
             Eigen::RowVectorXui group_read_counts = read_counts;
             collapseProbabilityMatrixReads(&group_read_path_probs, &group_read_counts);
